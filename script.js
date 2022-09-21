e3769dc3d249bb1695e49ba0e470e66486f65485
P-AlejandroJimenez <110042398+P-AlejandroJimenez@users.noreply.github.com>
2022-09-21 06:51:28 -0600

    The index.html file was cleaned up, the css was modified and developed, and the javascript file has been started but needs considerable work.

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
+var highscoreButton = document.querySelector(".highscores");
 var startButton = document.querySelector(".start-button");
 var timerElement = document.querySelector(".time-Remaining");
-
+var question = document.querrySelector
+ 
 var timer;
 var timeRemaining;
+var questionCount = 0;
 var correctCount = 0;
 var incorrectCount = 0;
 
 
-
 function startQuiz () {
-    timeRemaining = 100;
-    startTimer()
+    timeRemaining = 60;
+    startTimer();
+    queQuestion ();
 }
 
 function startTimer() {
     timer = setInterval(function() {
         timeRemaining--;
-        timerElement.textContent = timeRemaining;
+        timerElement.textContent = ` Seconds Remaining:${ timeRemaining}`;
         if (timeRemaining === 0)
             clearInterval(timer);
     }, 1000);
 }
 
+function queQuestion ()  {
+
+}
+
+function answerSelected () {
+
+}
+//saveScoreButton.addEventListener("click", function (event) {
+    //section 22 for the rest on local storage
+//}
+
+
 startButton.addEventListener("click", startQuiz);
+
